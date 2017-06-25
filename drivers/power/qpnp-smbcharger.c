22c5a26f6482523a43e16f1efcb2f40c4cc1f237
Morgan Lee <morganleezd2003@googlemail.com>
2017-06-25 11:42:00 +0800

    Allow sysfs charge rate control
     Author:    Morgan Lee <morganleezd2003@googlemail.com>
     Date:      Sun Jun 25 11:42:00 2017 +0800

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -453,7 +453,7 @@ module_param_named(
 static int smbchg_default_hvdcp_icl_ma = 1200;
 module_param_named(
 	default_hvdcp_icl_ma, smbchg_default_hvdcp_icl_ma,
-	int, S_IRUSR | S_IWUSR
+	int, 0664
 );
 
 static int smbchg_default_hvdcp3_icl_ma = 2000;
@@ -465,7 +465,7 @@ module_param_named(
 static int smbchg_default_dcp_icl_ma = 2000;
 module_param_named(
 	default_dcp_icl_ma, smbchg_default_dcp_icl_ma,
-	int, S_IRUSR | S_IWUSR
+	int, 0664
 );
 
 static int wipower_dyn_icl_en;
