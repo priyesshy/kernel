102b940bb5c11c19ce45b9080f3bcca5094c15ec
Unknown <gamingdevilscc@gmail.com>
2018-03-20 20:55:43 -0400

    add new pm kernel driver v1.7.1 for android powersuspend
    Signed-off-by: Haseo97 <jawgattus@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -29,6 +29,9 @@
 #include "mdss_dsi.h"
 #ifdef TARGET_HW_MDSS_HDMI
 #include "mdss_dba_utils.h"
+
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
 #endif
 
 
@@ -888,6 +891,12 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 	}
 
     display_on = true;
+#ifdef CONFIG_POWERSUSPEND
+       set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
+#endif
+
+	display_on = true;
+
 	pinfo = &pdata->panel_info;
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
@@ -1038,6 +1047,12 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 		mdss_dsi_panel_cmds_send(ctrl, &ctrl->off_cmds, CMD_REQ_COMMIT);
 
 	mdss_dsi_panel_off_hdmi(ctrl, pinfo);
+
+#ifdef CONFIG_POWERSUSPEND
+       set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
+#endif
+
+	display_on = false;
 
 end:
 	/* clear idle state */
--- kernel/power/powersuspend.c
+++ kernel/power/powersuspend.c
@@ -16,6 +16,15 @@
  *
  *  v1.5 - fix hybrid-kernel mode cannot be set through sysfs
  *
+<<<<<<< HEAD
+=======
+ *  v1.6 - remove autosleep and hybrid modes (autosleep not working on shamu)
+ *
+ *  v1.7 - do only run state change if change actually requests a new state
+ *
+ * v1.7.1 - Add autosleep and hybrid modes back
+ *
+>>>>>>> 62f3de7... add new pm kernel driver for android powersuspend
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
  * may be copied, distributed, and modified under those terms.
@@ -33,9 +42,14 @@
 #include <linux/workqueue.h>
 
 #define MAJOR_VERSION	1
+<<<<<<< HEAD
 #define MINOR_VERSION	5
 
 #define POWER_SUSPEND_DEBUG // Add debugging prints in dmesg
+=======
+#define MINOR_VERSION	7
+#define SUB_MINOR_VERSION 1
+>>>>>>> 62f3de7... add new pm kernel driver for android powersuspend
 
 struct workqueue_struct *suspend_work_queue;
 
@@ -78,7 +92,11 @@ static void power_suspend(struct work_struct *work)
 	unsigned long irqflags;
 	int abort = 0;
 
+<<<<<<< HEAD
 	#ifdef POWER_SUSPEND_DEBUG
+=======
+	#ifdef CONFIG_POWERSUSPEND_DEBUG
+>>>>>>> 62f3de7... add new pm kernel driver for android powersuspend
 	pr_info("[POWERSUSPEND] entering suspend...\n");
 	#endif
 	mutex_lock(&power_suspend_lock);
@@ -90,7 +108,11 @@ static void power_suspend(struct work_struct *work)
 	if (abort)
 		goto abort_suspend;
 
+<<<<<<< HEAD
 	#ifdef POWER_SUSPEND_DEBUG
+=======
+	#ifdef CONFIG_POWERSUSPEND_DEBUG
+>>>>>>> 62f3de7... add new pm kernel driver for android powersuspend
 	pr_info("[POWERSUSPEND] suspending...\n");
 	#endif
 	list_for_each_entry(pos, &power_suspend_handlers, link) {
@@ -98,7 +120,11 @@ static void power_suspend(struct work_struct *work)
 			pos->suspend(pos);
 		}
 	}
+<<<<<<< HEAD
 	#ifdef POWER_SUSPEND_DEBUG
+=======
+	#ifdef CONFIG_POWERSUSPEND_DEBUG
+>>>>>>> 62f3de7... add new pm kernel driver for android powersuspend
 	pr_info("[POWERSUSPEND] suspend completed.\n");
 	#endif
 abort_suspend:
@@ -111,7 +137,11 @@ static void power_resume(struct work_struct *work)
 	unsigned long irqflags;
 	int abort = 0;
 
+<<<<<<< HEAD
 	#ifdef POWER_SUSPEND_DEBUG
+=======
+	#ifdef CONFIG_POWERSUSPEND_DEBUG
+>>>>>>> 62f3de7... add new pm kernel driver for android powersuspend
 	pr_info("[POWERSUSPEND] entering resume...\n");
 	#endif
 	mutex_lock(&power_suspend_lock);
@@ -123,7 +153,11 @@ static void power_resume(struct work_struct *work)
 	if (abort)
 		goto abort_resume;
 
+<<<<<<< HEAD
 	#ifdef POWER_SUSPEND_DEBUG
+=======
+	#ifdef CONFIG_POWERSUSPEND_DEBUG
+>>>>>>> 62f3de7... add new pm kernel driver for android powersuspend
 	pr_info("[POWERSUSPEND] resuming...\n");
 	#endif
 	list_for_each_entry_reverse(pos, &power_suspend_handlers, link) {
@@ -131,17 +165,27 @@ static void power_resume(struct work_struct *work)
 			pos->resume(pos);
 		}
 	}
+<<<<<<< HEAD
 	#ifdef POWER_SUSPEND_DEBUG
+=======
+	#ifdef CONFIG_POWERSUSPEND_DEBUG
+>>>>>>> 62f3de7... add new pm kernel driver for android powersuspend
 	pr_info("[POWERSUSPEND] resume completed.\n");
 	#endif
 abort_resume:
 	mutex_unlock(&power_suspend_lock);
 }
 
+<<<<<<< HEAD
+=======
+bool power_suspended = false;
+
+>>>>>>> 62f3de7... add new pm kernel driver for android powersuspend
 void set_power_suspend_state(int new_state)
 {
 	unsigned long irqflags;
 
+<<<<<<< HEAD
 	spin_lock_irqsave(&state_lock, irqflags);
 	if (state == POWER_SUSPEND_INACTIVE && new_state == POWER_SUSPEND_ACTIVE) {
 		#ifdef POWER_SUSPEND_DEBUG
@@ -177,6 +221,51 @@ void set_power_suspend_state_panel_hook(int new_state)
 	pr_info("[POWERSUSPEND] panel resquests %s.\n", new_state == POWER_SUSPEND_ACTIVE ? "sleep" : "wakeup");
 	#endif
 	// Yank555.lu : Only allow panel hook changes in panel & hybrid mode
+=======
+	if (state != new_state) {
+		spin_lock_irqsave(&state_lock, irqflags);
+		if (state == POWER_SUSPEND_INACTIVE && new_state == POWER_SUSPEND_ACTIVE) {
+			#ifdef CONFIG_POWERSUSPEND_DEBUG
+			pr_info("[POWERSUSPEND] state activated.\n");
+			#endif
+			state = new_state;
+		power_suspended = true;
+			queue_work(suspend_work_queue, &power_suspend_work);
+		} else if (state == POWER_SUSPEND_ACTIVE && new_state == POWER_SUSPEND_INACTIVE) {
+			#ifdef CONFIG_POWERSUSPEND_DEBUG
+			pr_info("[POWERSUSPEND] state deactivated.\n");
+			#endif
+			state = new_state;
+		power_suspended = false;
+			queue_work(suspend_work_queue, &power_resume_work);
+		}
+		spin_unlock_irqrestore(&state_lock, irqflags);		
+	#ifdef CONFIG_POWERSUSPEND_DEBUG
+	} else {
+		pr_info("[POWERSUSPEND] state change requested, but unchanged ?! Ignored !\n");
+	#endif
+	}
+}
+
+void set_power_suspend_state_autosleep_hook(int new_state)		
+{		
+	#ifdef POWER_SUSPEND_DEBUG		
+	pr_info("[POWERSUSPEND] autosleep resquests %s.\n", new_state == POWER_SUSPEND_ACTIVE ? "sleep" : "wakeup");		
+	#endif		
+	// Yank555.lu : Only allow autosleep hook changes in autosleep & hybrid mode		
+	if (mode == POWER_SUSPEND_AUTOSLEEP || mode == POWER_SUSPEND_HYBRID)		
+		set_power_suspend_state(new_state);		
+}		
+		
+EXPORT_SYMBOL(set_power_suspend_state_autosleep_hook);	
+
+void set_power_suspend_state_panel_hook(int new_state)
+{
+	#ifdef CONFIG_POWERSUSPEND_DEBUG
+	pr_info("[POWERSUSPEND] panel resquests %s.\n", new_state == POWER_SUSPEND_ACTIVE ? "sleep" : "wakeup");
+	#endif
+	// Yank555.lu : Only allow autosleep hook changes in autosleep & hybrid mode
+>>>>>>> 62f3de7... add new pm kernel driver for android powersuspend
 	if (mode == POWER_SUSPEND_PANEL || mode == POWER_SUSPEND_HYBRID)
 		set_power_suspend_state(new_state);
 }
@@ -202,7 +291,11 @@ static ssize_t power_suspend_state_store(struct kobject *kobj,
 
 	sscanf(buf, "%d\n", &new_state);
 
+<<<<<<< HEAD
 	#ifdef POWER_SUSPEND_DEBUG
+=======
+	#ifdef CONFIG_POWERSUSPEND_DEBUG
+>>>>>>> 62f3de7... add new pm kernel driver for android powersuspend
 	pr_info("[POWERSUSPEND] userspace resquests %s.\n", new_state == POWER_SUSPEND_ACTIVE ? "sleep" : "wakeup");
 	#endif
 	if(new_state == POWER_SUSPEND_ACTIVE || new_state == POWER_SUSPEND_INACTIVE)
@@ -249,7 +342,11 @@ static struct kobj_attribute power_suspend_mode_attribute =
 static ssize_t power_suspend_version_show(struct kobject *kobj,
 		struct kobj_attribute *attr, char *buf)
 {
+<<<<<<< HEAD
 	return sprintf(buf, "version: %d.%d\n", MAJOR_VERSION, MINOR_VERSION);
+=======
+	return sprintf(buf, "version: %d.%d.%d\n", MAJOR_VERSION, MINOR_VERSION, SUB_MINOR_VERSION);
+>>>>>>> 62f3de7... add new pm kernel driver for android powersuspend
 }
 
 static struct kobj_attribute power_suspend_version_attribute =
@@ -300,10 +397,15 @@ static int __init power_suspend_init(void)
 		return -ENOMEM;
 	}
 
+<<<<<<< HEAD
 //	mode = POWER_SUSPEND_AUTOSLEEP;	// Yank555.lu : Default to autosleep mode
 //	mode = POWER_SUSPEND_USERSPACE;	// Yank555.lu : Default to userspace mode
 //	mode = POWER_SUSPEND_PANEL;	// Yank555.lu : Default to display panel mode
 	mode = POWER_SUSPEND_HYBRID;	// Yank555.lu : Default to display panel / autosleep hybrid mode
+=======
+//	mode = POWER_SUSPEND_USERSPACE;	// Yank555.lu : Default to userspace mode
+	mode = POWER_SUSPEND_PANEL;	// Yank555.lu : Default to display panel mode
+>>>>>>> 62f3de7... add new pm kernel driver for android powersuspend
 
 	return 0;
 }
@@ -323,4 +425,7 @@ MODULE_AUTHOR("Paul Reioux <reioux@gmail.com> / Jean-Pierre Rasquin <yank555.lu@
 MODULE_DESCRIPTION("power_suspend - A replacement kernel PM driver for"
         "Android's deprecated early_suspend/late_resume PM driver!");
 MODULE_LICENSE("GPL v2");
+<<<<<<< HEAD
 
+=======
+>>>>>>> 62f3de7... add new pm kernel driver for android powersuspend
