1413dabfd628ca16942ddc48e13ff432c35a5baa
Haseo97 <jawgattus@gmail.com>
2018-05-17 13:45:21 +0700

    drivers: power: Force maximum charging speed for any power supply
    * Based on https://github.com/Planet-X/android-kernel-zenfone3_advanced/commit/be2bc2902007fed01eb94d8fdbf37299147e8a97
    
    Signed-off-by: Haseo97 <jawgattus@gmail.com>

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -1714,6 +1714,19 @@ static int smbchg_set_usb_current_max(struct smbchg_chip *chip,
 		rc = vote(chip->usb_suspend_votable, USB_EN_VOTER, false, 0);
 	}
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+	if(force_fast_charge > 0){
+		printk("[BAT][CHG] WARNING: Force fast charge enabled!");
+		current_ma = CURRENT_1500_MA;
+		rc = smbchg_set_high_usb_chg_current(chip, current_ma);
+		rc = smbchg_masked_write(chip, chip->usb_chgpth_base + CMD_IL,
+			ICL_OVERRIDE_BIT, ICL_OVERRIDE_BIT);
+		if (rc < 0)
+			pr_err("Couldn't set ICL override rc = %d\n", rc);
+		goto out;
+	}
+#endif
+
 	switch (chip->usb_supply_type) {
 	case POWER_SUPPLY_TYPE_USB:
 		if ((current_ma < CURRENT_150_MA) &&
@@ -1805,12 +1818,7 @@ static int smbchg_set_usb_current_max(struct smbchg_chip *chip,
 			}
 			chip->usb_max_current_ma = 500;
 		}
-#ifdef CONFIG_FORCE_FAST_CHARGE
-		if ((force_fast_charge > 0) &&
-			(current_ma == CURRENT_500_MA || current_ma == CURRENT_900_MA)) {
-#else
 		if (current_ma == CURRENT_900_MA) {
-#endif
 			rc = smbchg_sec_masked_write(chip,
 					chip->usb_chgpth_base + CHGPTH_CFG,
 					CFG_USB_2_3_SEL_BIT, CFG_USB_3);
